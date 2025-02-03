19a31aa7556490613379425353bf5d12be4ecda8
Nikolai E <starnuik@gmail.com>
2025-02-03 21:25:46 +0300

    убран sqlite из DbOptions

--- Notissimus.PointerTracking.Infrastructure/DbOptions.cs
+++ Notissimus.PointerTracking.Infrastructure/DbOptions.cs
@@ -5,12 +5,6 @@ namespace Notissimus.PointerTracking.Infrastructure;
 
 public static class DbOptions
 {
-    public static void SetSqlite(DbContextOptionsBuilder builder, IConfiguration config)
-    {
-        builder.UseSqlite($"Data Source=../db.sqlite", 
-            b => b.MigrationsAssembly("Notissimus.PointerTracking.Migrations"));
-    }
-    
     public static void SetPostgres(DbContextOptionsBuilder builder, IConfiguration config)
     {
         builder.UseNpgsql(
